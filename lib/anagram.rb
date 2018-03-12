970ecf88254f9e1d48bccd4c55890054757e39ac
Mark Cruz <markchriscruz@yahoo.com>
2018-03-12 05:27:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,16 @@ class Anagram
   end
   
   def match(array)
-    
+    array.each do |word|
+      array_of_letters = word.split("")
+      array_of_letters = array_of_letters.sort
+      word_sorted = array_of_letters.join
+      if word_sorted == @name_sorted
+        return word
+      else
+        #do nothing
+      end
+    end
   end
   
 end
