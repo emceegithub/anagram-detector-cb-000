543a87e6a78b23f62e2ed732465ba3ead9c08385
Mark Cruz <markchriscruz@yahoo.com>
2018-03-12 05:33:56 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,16 +6,20 @@ class Anagram
   end
   
   def match(array)
+    collector = []
     array.each do |word|
+      #puts word
       array_of_letters = word.split("")
       array_of_letters = array_of_letters.sort
       word_sorted = array_of_letters.join
+      #puts word_sorted
       if word_sorted == @name_sorted
-        return word
+        collector << word
       else
         #do nothing
       end
     end
+    return collector
   end
   
 end
